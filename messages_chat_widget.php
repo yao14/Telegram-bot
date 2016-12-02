3f922abf797fea15961007083b276348edcc3ab9
sampsonyao <yao.sampson@st.vvu.edu.gh>
2016-12-02 01:32:25 +0100

    updated

--- messages_chat_widget.php
+++ messages_chat_widget.php
@@ -10,11 +10,55 @@
     <script src="jquery.min.js"></script>
     <script src="bootstrap.min.js"></script>
     <link href="bootstrap.css" rel="stylesheet">
+    <link href="font-awesome.min.css" rel="stylesheet">
 
     <style type="text/css">
     	body{
     
     background:#ebeef0;
+}
+
+.media-block .media-left {
+    display: block;
+    float: left;
+}
+
+.speech {
+    position: relative;
+    background: #b7dcfe;
+    color: #317787;
+    display: inline-block;
+    border-radius: 0;
+    padding: 12px 20px;
+}
+
+.media-block .media-left {
+    display: block;
+    float: left;
+}
+.img-sm {
+    width: 46px;
+    height: 46px;
+}
+
+.pad-hor {
+    padding-left: 15px;
+    padding-right: 15px;
+}
+.speech .media-heading {
+    font-size: 1.2em;
+    color: #317787;
+    display: block;
+    border-bottom: 1px solid rgba(0,0,0,0.1);
+    margin-bottom: 10px;
+    padding-bottom: 5px;
+    font-weight: 300;
+}
+.speech-time {
+    margin-top: 20px;
+    margin-bottom: 0;
+    font-size: .8em;
+    font-weight: 300;
 }
 .panel {
     box-shadow: 0 2px 0 rgba(0,0,0,0.075);
@@ -53,6 +97,9 @@
 .panel-control .btn, .panel-control .dropdown-toggle.btn {
     border: 0;
 }
+.mar-btm {
+    margin-bottom: 15px;
+}
     </style>
 </head>
 <body>
@@ -90,13 +137,47 @@
     		
     		<div id="demo-chat-body" class="collapse in">
     			<div class="nano has-scrollbar" style="height:380px">
-    				
+    				<div class="nano-content pad-all" tabindex="0" style="<!-- right: -17px -->;">
+                        <ul class="list-unstyled media-block">
+                            
+                           
+
+                            <li class="mar-btm">
+                                <div class="media-left">
+                                    <img src="bot.jpg" class="img-circle img-sm" alt="Profile Picture">
+                                </div>
+                                <div class="media-body pad-hor speech-left">
+                                    <div class="speech">
+                                        <a href="#" class="media-heading">server</a>
+                                        
+                                        <p class="speech-time">
+                                            <i class="fa fa-clock-o fa-fw"></i> time
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            
+                            
+                            
+                            
+                            
+                            
+                        </ul>
+                    </div>
     			<div class="nano-pane"><div class="nano-slider" style="height: 141px; transform: translate(0px, 0px);"></div>
                 </div>
                 </div>
     
     			<!--Widget footer-->
     			<div class="panel-footer">
+                <div class="row">
+                        <div class="col-xs-9">
+                            <input name="input" type="text" placeholder="Enter your text" class="form-control chat-input" style="height: auto;">
+                        </div>
+                        <div class="col-xs-3">
+                            <button class="btn btn-primary btn-block" type="submit" name="submit">Send</button>
+                        </div>
+                    </div>
     				
     			</div>
     		</div>
